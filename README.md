2683c14c8df523c096ae28232a515639eb1ef83f
desarrollo <joseluisgm1998@gmail.com>
2024-06-19 14:23:19 -0500

    se agrego la conexion con la base de datos

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # fastapi-tools
+
+
+##variables de entorno
+
+###base de datos
+mysql =  mysql+mysqldb://{user}:{password}@{ip}:{port}/{dba}
+DATABASE_URL_SQLITE = sqlite:///ecommerce.db
+####conexion con la base de datos con postgresql
+DATABASE_URL_POSTGRESQL = postgresql://{user}:{password}@{host}:{puerto}/{database}
+####conexion con la base de datos con oracle
+DATABASE_URL_ORACLE = oracle+cx_oracle://{user}:{password}@{host}:{puerto}/{database}
+####conexion con la base de datos con mssql
+DATABASE_URL_MSSQL = mssql+pyodbc://{user}:{password}@{host}:{puerto}/{database}
---
+++ core/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import settingsbd
+
+DATABASE_URL = settingsbd.DATABASE_URL_MYSQL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# Función para probar la conexión a la base de datos
+def test_db_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        print("Conexión a la base de datos exitosa.")
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+
+# Llama a la función para probar la conexión al iniciar el módulo
+test_db_connection()
+
+# Dependencia para obtener la sesión de la base de datos
+def get_db():
+    db = SessionLocal()
+    try:
+        return db
+    finally:
+        db.close()
